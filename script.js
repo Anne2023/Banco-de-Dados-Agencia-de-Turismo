3ead108935b60e54c4cd2f7485e526eab1670e73
Amábile Silva <123522722+Anne2023@users.noreply.github.com>
2023-05-15 19:31:34 -0300

    SD

---
+++ script.js
@@ -0,0 +1,12 @@
+
+<><script src="js/jquery.js"></script><script>
+
+    $(function() {$(".toggle").on("click", function () {
+        if ($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+        } else {
+            $(".item").addClass("active");
+        }
+    })};
+    );
+</script></>
